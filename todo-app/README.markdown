660817f7274ff4ba44e293e8a6403935470968ef
Venu <you@example.com>
2019-08-26 22:44:39 +0530

    my first jquery code

---
+++ todo-app/README.markdown
@@ -0,0 +1,4 @@
+# todo app
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/venujibhakate/pen/VwZmNmP](https://codepen.io/venujibhakate/pen/VwZmNmP).
+
+ 
---
+++ todo-app/src/script.js
@@ -0,0 +1,26 @@
+$(".txtb").on("keyup" , function(e){
+//     13 means enter botton
+    if(e.keyCode == 13 && $(".txtb").val() !=  "")
+      {
+       var task = $("<div class='task'></div>").text($(".txtb").val());
+       var del = $("<i class='fas fa-trash-alt'></i>").click(function(){
+            task.hide();
+          
+        });
+          var check = $("<i class='fas fa-check'></i>").click(function(){
+            $(".comp").append(task);
+          });
+       var edit =  $("<i class='fa fa-edit'></i>").click(function(){
+        
+        $(".txtb").val(task.text());
+         task.hide();
+    });
+        
+        
+          
+        task.append(del,check,edit);
+        
+        $(".notcomp").append(task);
+}
+});
+
